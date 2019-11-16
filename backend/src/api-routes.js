a972c6bf27655a9746598b8f0e8cca7c32441897
Benjamin Wittke <wittke.benjamin@web.de>
2019-11-16 16:27:47 +0100

    read single and multiple conference Objects

--- backend/src/api-routes.js
+++ backend/src/api-routes.js
@@ -1,12 +1,13 @@
 // Filename: api-routes.js
 // Initialize express router
 let router = require('express').Router();
+let ObjectId = require('mongodb').ObjectId;
+
 // Import Mongo
 const MongoClient = require("mongodb").MongoClient;
 const CONNECTION_URL = "mongodb+srv://Herta:PKC5ZLoLNcg2zEez@kangoonet-entqn.gcp.mongodb.net/test?retryWrites=true&w=majority";
 const DATABASE_NAME = "KangooNet";
 var db;
-//connect mongoDB
 
 
 // Set default API response
@@ -22,13 +23,29 @@ router.get('/user', function (req, res) {
         message: 'Welcome to KangooNets world of APIs'
     });
 });
-
+//get all conferences
 router.get('/conference', function (req, res) {
+
     MongoClient.connect(CONNECTION_URL, function (err, client) {
-        db = client.db(DATABASE_NAME).collection("conference").find({}, function (err, result) {
-            if (err) throw err;
+        result = client.db(DATABASE_NAME).collection("conference").find({}).toArray(function (err, result) {
             console.log(result);
-            res.send("jo!");
+            res.send(result);
+            client.close();
+        })
+
+
+    });
+});
+//get one conference with ID
+router.get('/conference/:id', function (req, res) {
+
+    const id = parseInt(req.params.id, 10);
+    console.log("ID" + id);
+    var objID = new ObjectId("5dd011114ca4823874e9fefa")
+    MongoClient.connect(CONNECTION_URL, function (err, client) {
+        client.db(DATABASE_NAME).collection("conference").findOne({_id: objID}, function (err, result) {
+            console.log(result);
+            res.send(result);
             client.close();
         })
     });
