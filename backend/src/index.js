95a3528d3459c21912c36a4a9928a201239958e7
Manuel Truckses <manuel.truckses@mail.schwarz>
2019-11-16 14:01:41 +0100

    add mongoDB, swagger

---
+++ backend/src/index.js
@@ -0,0 +1,30 @@
+// FileName: index.js
+// Import express
+const express = require('express');
+// Inpmort body-parser
+const BodyParser = require("body-parser");
+// Import Swagger
+const swaggerUi = require("swagger-ui-express");
+swaggerDocument = require('./swagger.json');
+// Import Mongo
+const  MongoClient = require("mongodb").MongoClient;
+const CONECTION_URL ="mongodb+srv://Herta:PKC5ZLoLNcg2zEez@kangoonet-entqn.gcp.mongodb.net/test?retryWrites=true&w=majority";
+const DATABASE_NAME = "KangooNet";
+// Initialize the app
+const app = express();
+
+app.use(BodyParser.json());
+app.use(BodyParser.urlencoded({ exended: true }));
+// Setup server port
+var port = process.env.PORT || 8080;
+// Send message for default URL
+app.get('/', (req, res) => res.send('Hello World with Express'));
+// Launch app to listen to specified port
+app.listen(port, function () {
+    console.log("Running RestHub on port " + port);
+});
+// Import routes
+let apiRoutes = require("./api-routes");
+// Use Api routes in the App
+app.use('/api', apiRoutes);
+app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
